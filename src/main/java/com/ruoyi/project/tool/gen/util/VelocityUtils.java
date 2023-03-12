8271af1fe5f09495e55b269744b3f2b505b7d525
gaoweishan@yuqiaotech.com <konnysnow@163.com>
2023-03-12 14:54:51 +0800

    1，controller模版的add方法调用edit页面 2。serviceImpl里，update方法改为update Or Insert 3。不生成add页面

--- src/main/java/com/ruoyi/project/tool/gen/util/VelocityUtils.java
+++ src/main/java/com/ruoyi/project/tool/gen/util/VelocityUtils.java
@@ -143,7 +143,7 @@ public class VelocityUtils
             templates.add("vm/html/list.html.vm");
             templates.add("vm/java/sub-domain.java.vm");
         }
-        templates.add("vm/html/add.html.vm");
+        //templates.add("vm/html/add.html.vm");//不用add页面了！！
         templates.add("vm/html/edit.html.vm");
         templates.add("vm/sql/sql.vm");
         return templates;
